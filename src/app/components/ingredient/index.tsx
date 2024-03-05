04718db974724febc1c66510ebc6ee48ec2ab297
Jorge Cruz <j.alexcruz@gmail.com>
2024-03-05 18:17:58 -0300

    primeira funcionalidade implementada, listar escolhas, selecionar e load scrollview

--- src/app/components/ingredient/index.tsx
+++ src/app/components/ingredient/index.tsx
@@ -1,11 +1,18 @@
-import { Pressable, Text, Image } from 'react-native';
+import { Pressable, PressableProps, Text, Image } from 'react-native';
 import { styles } from './styles'
 
-export default function Ingredient() {
+//Propriedades do Pressable
+export type IngredientsProps = {
+  name: string,
+  image:string,
+  selected:boolean
+}
+export default function Ingredient({name, image, selected = false, ...rest}: IngredientsProps & PressableProps) {
+
  return (
-   <Pressable style={styles.container}>
-    <Image source={require("@/src/assets/tomato.png")} style={styles.image}/>
-        <Text style={styles.title}>Maça</Text>
+   <Pressable style={[styles.container, selected && styles.selected]} {...rest}>
+      <Image source={require("@/src/assets/tomato.png")} style={styles.image}/>
+      <Text style={styles.title}>Maça</Text>
    </Pressable>
   );
 }
--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import { styles } from './styles';
 import Ingredient from '../components/ingredient';
 
 export default function Index() {
+
+//estado do pressable  
+const [selected, setSeleted] = useState<string[]>([])
+
+function handleToggleSelected(value:string){
+  if(selected.includes(value)) {
+    return setSeleted((state) => state.filter((item) => item !== value))
+  }
+
+  setSeleted((state) => [...state, value])
+}
+
  return (
    <View style={styles.container}>
         <Text style={styles.title}>
@@ -16,9 +29,13 @@ export default function Index() {
         <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.ingredients}>
             {
               Array.from({ length:100 }).map((item, index) => (
-                <Ingredient key={index}/>
-              ))
-            }
+                <Ingredient 
+                key={index} 
+                name=""
+                image="" 
+                selected={selected.includes(String(index))}
+                onPress={() => handleToggleSelected(String(index))}/>
+            ))}
     
         </ScrollView>
       
